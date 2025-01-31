ffb412b938ecab3f5b5dc20f519d45b399122c27
Sandeep Santhosh <sandepsanthosh@gmail.com>
2025-01-31 12:05:11 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 import Navbar from './components/Navbar'
-import LoginPage from './pages/login/LoginPage';
+import LoginPage from './pages/LoginPage';
 import { useThemeStore } from "./store/useThemeStore";
 const App = () => {
   const { theme } = useThemeStore();
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ const LoginPage = () => {
   return (
       <div className="min-h-screen flex items-center justify-center bg-[#2D2424]">
       <div className="flex bg-[#3E3232] text-white rounded-2xl shadow-lg w-[850px] h-[450px] overflow-hidden">
-        {/* Left Side: Login Form */}
+
         <div className="w-1/2 px-10 py-8 flex flex-col justify-center">
           <h2 className="text-3xl font-bold text-center mb-3" style={{ fontFamily: 'Product Sans, sans-serif' }}>
             Welcome Back
@@ -46,7 +46,7 @@ const LoginPage = () => {
           </p>
         </div>
 
-        {/* Right Side: Image */}
+
         <div className="w-1/2">
           <img 
             src="https://i.pinimg.com/736x/89/6d/59/896d59d3c6fc8f1967959793da64e828.jpg" 
