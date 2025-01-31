401516d78c26bb82a1961ce81c049335a5578480
Sandeep Santhosh <sandepsanthosh@gmail.com>
2025-01-31 12:12:28 +0530

    sda

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 
 import Navbar from './components/Navbar'
-import LoginPage from './pages/LoginPage';
 import { useThemeStore } from "./store/useThemeStore";
 const App = () => {
   const { theme } = useThemeStore();
   return (
     <div data-theme={theme}>
     <Navbar/>
-    <LoginPage/>
     </div>
   )
 }
--- frontend/src/pages/SettingPage.jsx
+++ frontend/src/pages/SettingPage.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const SettingPage = () => {
   return (
     <div>SettingPage</div>
