2e9a4e32a1e028349ad27aed7d370dbf4f999aaa
Robin Samuelkutty <robinsam754@gmail.com>
2025-02-07 14:40:37 +0530

    message setup started

---
+++ frontend/src/components/skeltons/MessageSkelton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MessageSkelton = () => {
+  return (
+    <div>MessageSkelton</div>
+  )
+}
+
+export default MessageSkelton
---
+++ frontend/src/components/skeltons/SidebarSkelton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SidebarSkelton = () => {
+  return (
+    <div>SidebarSkelton</div>
+  )
+}
+
+export default SidebarSkelton
